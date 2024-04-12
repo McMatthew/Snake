6f98c167a2ae377d143bff6f72a37878b0c8d5ee
Vincenzo Arnone <v.arnone.mail@gmail.com>
2024-04-12 10:29:37 +0200

    test_placingHead_shouldBeThere

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
 # Snake <3
+import unittest
+from World import SWorld
 
-class Tests()
-    if __name__ == '__main__'
+class Tests(unittest.TestCase):
+    def test_placingHead_shouldBeThere(self):
+        world = SWorld()
+        world.placeSnake(0, 0)
+
+        self.assertEqual(world.getSnakePosition(), (0, 0))
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ World.py
@@ -0,0 +1,6 @@
+class SWorld:
+    def getSnakePosition(self)->tuple[int,int]:
+        return (0, 0)
+    
+    def placeSnake(self, x: int, y: int):
+        pass
